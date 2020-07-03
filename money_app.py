a92eea2c22c5a2774ce825cc9b809e8c6339592d
elc17 <neto@lopezcenteno.com>
2020-07-02 20:00:13 -0600

    Value

--- money_app.py
+++ money_app.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 from password import password
 
-engine = create_engine(f'postgresql+psycopg2://postgres:{password}@localhost:5432/sportSalay_db')
+engine = create_engine(f'postgres://rwbdompjejjwby:c2e61aca98786bb3402c23494beb1902d523ef00dc2011cb46381334bb93146a@ec2-34-224-229-81.compute-1.amazonaws.com:5432/d1i0ak4t8u1rei')
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
