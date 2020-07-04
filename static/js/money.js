633573e7ab509532b0a3f9d814492babba027842
JaviSandoval94 <javier.sandoval94@outlook.com>
2020-07-03 20:44:57 -0500

    Added sunburst chart

--- static/js/money.js
+++ static/js/money.js
@@ -8,75 +8,79 @@ url = "/api/sports"
 var data = d3.json(url);
 
 function radialChart(url){
-
-  // set the dimensions and margins of the graph
-  var margin = {top: 100, right: 0, bottom: 0, left: 0},
-      width = 460 - margin.left - margin.right,
-      height = 460 - margin.top - margin.bottom,
-      innerRadius = 90,
-      outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
-  
-  // append the svg object
-  var svg = d3.select("#my_dataviz")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
-
   d3.json(url).then( (sample) => {
+    function add(accumulator, a) {
+      return accumulator + a;
+    }
+    var sportsEarnings = sample.yearSport;
+    var yearSport = [];
+    var sports = [];
+    Object.entries(sportsEarnings).forEach(([key, value]) => {
+      yearSport.push(key);
+      sports.push(value);
+    });
+    var sportArray = [];
+    var earningArray = [];
+    sports.forEach( (sport) => {
+      Object.entries(sport).forEach(([key, value]) => {
+        sportArray.push(key);
+        earningArray.push(Number.parseInt(value));
+      });
+    });
 
-    var data = [];
-    var nationGlobal = sample.nationGlobalE;
+    // Call the corresponding part of JSON
+    var yearaEarnings = sample.yearEarnings;
+    var year = [];
+    var earnings = [];
+    Object.entries(yearaEarnings).forEach(([key, value]) => {
+      year.push(key);
+      earnings.push(parseInt(value["Earnings (Millions of US$)"]));
+    });
+    sumTotal = earnings.reduce(add,0);
     
-    Object.entries(nationGlobal).forEach(([key, value]) => {
-        var myObject = new Object();
-        myObject.Country = key;
-        myObject.Value = parseInt(value["Earnings (Millions of US$)"]);
-        // var myString = `{Country: ${key}, Value: ${parseInt(value["Earnings (Millions of US$)"])}}`;
-        data.push(myObject);
-            
-    });        
+    var labels = ["Total"];
+    var parents = [""];
+    var values = [sumTotal];
+    year.forEach( (value) => {
+      labels.push(value);
+      parents.push("Total");
+    });
+    earnings.forEach( (value) => {
+      values.push(value);
+    });
+    earningArray.forEach( (value) => {
+      values.push(value);
+    });
+    sportArray.forEach( (value) => {
+      labels.push(value);
+    });
+    year.forEach( (value) => {
+      for (var i =0; i <13; i++) {
+        parents.push(value);
+      };
+    });
 
- 
-  // Scales
-  var x = d3.scaleBand()
-      .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
-      .align(0)                  // This does nothing
-      .domain(data.map(function(d) { return d.Country; })); // The domain of the X axis is the list of states.
-  var y = d3.scaleRadial()
-      .range([innerRadius, outerRadius])   // Domain will be define later.
-      .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
-
-  // Add the bars
-  svg.append("g")
-    .selectAll("path")
-    .data(data)
-    .enter()
-    .append("path")
-      .attr("fill", "#69b3a2")
-      .attr("d", d3.arc()     // imagine your doing a part of a donut plot
-          .innerRadius(innerRadius)
-          .outerRadius(function(d) { return y(d['Value']); })
-          .startAngle(function(d) { return x(d.Country); })
-          .endAngle(function(d) { return x(d.Country) + x.bandwidth(); })
-          .padAngle(0.01)
-          .padRadius(innerRadius))
-
-  // Add the labels
-  svg.append("g")
-      .selectAll("g")
-      .data(data)
-      .enter()
-      .append("g")
-        .attr("text-anchor", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-        .attr("transform", function(d) { return "rotate(" + ((x(d.Country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Value'])+10) + ",0)"; })
-      .append("text")
-        .text(function(d){return(d.Country)})
-        .attr("transform", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-        .style("font-size", "11px")
-        .attr("alignment-baseline", "middle")
-
+    console.log(labels);
+    console.log(parents);
+    console.log(values);
+    var data = [{
+      "type": "sunburst",
+      "labels": labels,
+      "parents": parents,
+      "values": values,
+      "leaf": {"opacity": 0.4},
+      "marker": {"line": {"width": 2}},
+      "branchvalues": 'total'
+    }];
+    
+    var layout = {
+      "margin": {"l": 0, "r": 0, "b": 0, "t": 0},
+    };
+    
+    
+    Plotly.newPlot('my_sunburst', data, layout, {showSendToCloud: true})
+    
+    myPlot = document.getElementById("my_sunburst");
   });
 }
 
